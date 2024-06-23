317613e371665ad6ff509b0a3bec5bfe7fc46b4c
shivam <shivamkatyansk@gmail.com>
2024-06-23 16:40:44 +0530

    db variables update for localhost

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+const sequelize = new Sequelize('shoppy-node', 'user', 'root', {
   dialect: 'mysql',
   host: 'localhost'
 });
