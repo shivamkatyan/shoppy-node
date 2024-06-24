35351c7cdff72604efcde88e8ece7fecc0ef1887
shivam <shivamkatyansk@gmail.com>
2024-06-24 17:34:20 +0530

    updated db files

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+const errorController = require("./controllers/error");
+const mongoConnect = require("./util/database").mongoConnect;
+const User = require("./models/user");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findById('5baa2528563f16379fc8a610')
-    .then(user => {
+  User.findById("66795b305ea12a23e6f998b9")
+    .then((user) => {
       req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 });
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
--- util/database.js
+++ util/database.js
@@ -1,18 +1,19 @@
-const mongodb = require('mongodb');
+const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
 let _db;
 
-const mongoConnect = callback => {
+const mongoConnect = (callback) => {
   MongoClient.connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+    "mongodb+srv://shoppy:W4t7TvOTa3y9Bzv9@shoppy.srreepn.mongodb.net/shoppy?retryWrites=true&w=majority&appName=shoppy",
+    { useNewUrlParser: true }
   )
-    .then(client => {
-      console.log('Connected!');
+    .then((client) => {
+      console.log("Connected!");
       _db = client.db();
       callback();
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       throw err;
     });
@@ -22,7 +23,7 @@ const getDb = () => {
   if (_db) {
     return _db;
   }
-  throw 'No database found!';
+  throw "No database found!";
 };
 
 exports.mongoConnect = mongoConnect;
